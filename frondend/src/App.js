52172b9cb4b5472e5fa3e6db79b7fc5dc8ea524e
V-sukumar <thalasuku2001@gmail.com>
2023-07-20 21:33:42 +0530

    day3

--- frondend/src/App.js
+++ frondend/src/App.js
@@ -6,6 +6,14 @@ function App() {
     <div className="App">
      <h1>Intial commit</h1>
      <h1>Suku</h1>
+     <h2>Create New Repository</h2>
+     <input palceholder="name of repository" type="text"></input>
+     <select>
+      <option></option>
+     </select>
+     <button>create</button>
+     <input palceholder="add description" type="text"></input>
+ 62c358f566ee9453e6a02868835f446d30b2438f
     </div>
   );
 }
---
+++ frondend/src/Pages/Notfound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Notfound = () => {
+  return (
+    <div>
+        <h1>Page Not Found 404  </h1>
+    </div>
+  )
+}
