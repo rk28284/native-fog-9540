8da23ba5e28bc2a449e0f16434f06a27bd51fd6d
V-sukumar <thalasuku2001@gmail.com>
2023-07-21 22:37:30 +0530

    day4

--- frondend/src/App.js
+++ frondend/src/App.js
@@ -1,19 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { Allroutes } from './Componet/Allroutes';
 
 function App() {
   return (
     <div className="App">
-     <h1>Intial commit</h1>
-     <h1>Suku</h1>
-     <h2>Create New Repository</h2>
-     <input palceholder="name of repository" type="text"></input>
-     <select>
-      <option></option>
-     </select>
-     <button>create</button>
-     <input palceholder="add description" type="text"></input>
- 62c358f566ee9453e6a02868835f446d30b2438f
+     <Allroutes/>
     </div>
   );
 }
