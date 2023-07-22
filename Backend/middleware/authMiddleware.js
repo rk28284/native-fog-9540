9337eec5453e1179e9a81022c78cfffdf1f81b1e
DevnandanRaj <devnandanraj07@gmail.com>
2023-07-22 20:25:11 +0530

    added blacklisting and logout

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,17 +1,23 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const RevokedToken = require("../models/revokedTokens");
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   try {
-    const token = req.header("Authorization");
+    const token = req.header('Authorization')
     if (!token) {
-      return res.status(401).json({ message: "Authorization token not found" });
+      return res.status(401).json({ message: 'Authorization token not found' });
     }
-
-    const decoded = jwt.verify(token, "codecollab");
+    // Check if the token is blacklisted
+    const isBlacklisted = await RevokedToken.exists({ token });
+    if (isBlacklisted) {
+      return res.status(401).json({ message: "Token revoked. Please log in again."});
+    }
+    // Verify the access token
+    const decoded = jwt.verify(token, 'Devnandan');
     req.user = { userId: decoded.userId };
     next();
   } catch (error) {
-    res.status(401).json({ message: "Invalid token" });
+    res.status(401).json({ message: 'Invalid token' });
   }
 };
 
